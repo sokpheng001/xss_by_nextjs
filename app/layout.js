285f2905278c062a05136a916701e2ff37f17c89
sokpheng001 <kimchansokpheng123@gmail.com>
2024-04-08 11:55:45 +0700

    Update SEO

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Localization",
   description: "Hack to get",
+  openGraph: {
+    title: "CSTAD Vulnerable Web Application Laboratory",
+    description:
+      "This is the lab for students to pratice XSS vulnerabilities attacks",
+    url: "http://136.228.158.126:50004/",
+    images: "https://store.istad.co/media/icon_images/ISTAD.jpg",
+  },
 };
 
 export default function RootLayout({ children }) {
--- app/login/page.js
+++ app/login/page.js
@@ -2,6 +2,13 @@ import FormLogin from "@/components/FormLogin";
 export const metadata = {
   title: "ISTAD - Vulnerable -  Login",
   description: "Hack to Learn",
+  openGraph: {
+    title: "CSTAD Vulnerable Login Laboratory",
+    description:
+      "This is the lab for students to pratice XSS vulnerabilities attacks",
+    url: "http://136.228.158.126:50004/",
+    images: "https://store.istad.co/media/icon_images/ISTAD.jpg",
+  },
 };
 export default function Page(props) {
   // bg-gradient-to-b from-cyan-100 to-white
